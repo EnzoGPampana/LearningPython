530159717a0783cc6b42379d5898956bba70a5e2
Enzo Garofalo Pampana <garofalo.enzo007@gmail.com>
2023-07-31 23:48:16 -0300

    Update Kata21.py

--- Kata21.py
+++ Kata21.py
@@ -1,28 +1,52 @@
-# Your job is to write a function which increments a string, to create a new string.
-# If the string already ends with a number, the number should be incremented by 1.
-# If the string does not end with a number. the number 1 should be appended to the new string.
-# Examples:
-# foo -> foo1
-# foobar23 -> foobar24
-# foo0042 -> foo0043
-# foo9 -> foo10
-# foo099 -> foo100
-# Attention: If the number has leading zeros the amount of digits should be considered.
+# Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+# array = [[1,2,3],
+#          [4,5,6],
+#          [7,8,9]]
+# snail(array) #=> [1,2,3,6,9,8,7,4,5]
+# For better understanding, please follow the numbers of the next array consecutively:
+# array = [[1,2,3],
+#          [8,9,4],
+#          [7,6,5]]
+# snail(array) #=> [1,2,3,4,5,6,7,8,9]
 
 
-strng = "foo"
+snail_map = [[1, 2, 3, 4, 5],
+           [6, 7, 8, 9, 10],
+           [11, 12, 13, 14, 15], 
+           [16, 17, 18, 19, 20],
+           [21, 22, 23, 24, 25]]
 
-def increment_string(strng):
 
-    if strng != int:
-        strng += "1"
+def snail(snail_map):
+    list_of_numbers = []
+    
+    while snail_map:
+    
+        for i in snail_map[0]:
+            list_of_numbers.append(i)
+        snail_map.pop(0)
+        
+        if not snail_map:
+            break
+                    
+        for j in snail_map:
+            list_of_numbers.append(j[-1])
+            j.pop()
             
+        for k in range(len(snail_map[-1]) -1, -1, -1):
+            list_of_numbers.append(snail_map[-1][k])
+        snail_map.pop()
 
-    return strng
+        if not snail_map:
+            break
+
+        for l in reversed(snail_map):
+            list_of_numbers.append((l[0]))
+            l.pop(0)
+        
+    return list_of_numbers
 
 
 
 
-
-
-print(increment_string(strng))
+print(snail(snail_map))
