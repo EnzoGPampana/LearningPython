e614e4175249d79ef690d4d2de4cd3f4ae3591a0
Enzo Garofalo Pampana <garofalo.enzo007@gmail.com>
2023-08-02 16:24:27 -0300

    Create AlarmClock.py

---
+++ 1 - Beginner/AlarmClock.py
@@ -0,0 +1,9 @@
+# Alarm Clock 
+# An alarm clock is a clock with a function that can be activated to ring at a time set in advance, used to wake someone up.
+
+
+wakeup_time = input("What time do you wanna wake up?\n[HH:MM:SS]\n")
+
+def goodmorning(wakeup_time):
+    return 0
+
